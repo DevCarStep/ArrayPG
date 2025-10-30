0cd0ce230050dd2de894d9cba203f3515375a573
CarStep <carstep9823@gmail.com>
2025-10-30 18:54:08 +0300

    Init

---
+++ ArrayPG/GenerationField.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPG
+{
+    public class GenerationField
+    {
+        public Element[,] generationArray;
+
+        public GenerationField(int height, int width)
+        {
+            generationArray = new Element[height, width];
+        }
+    }
+
+    public class Element
+    {
+        public Quad[] structure;
+    }
+
+    public class Quad : Element
+    {
+
+    }
+}
